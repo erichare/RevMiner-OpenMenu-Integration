407572a8a26297ef8cca0ea74bd7a51222fd6ed6
Eric Hare <Raftysworld@gmail.com>
2012-01-20 10:50:44 -0800

    Make the server code read from the OpenMenu xml file and parse it into a PHP struct

--- Server/test.php
+++ Server/test.php
@@ -1,10 +1,5 @@
 <?php
 
-/* 
-include "FINsert/config.php"; // get db credentials ...
-include "FINsert/opendb.php"; // ... which are used to open the db
-*/
-
 /*******************/
 	$dbhost = 'localhost'; //The host server of the database
 	$dbname = 'dawgsfor_openmenu'; //The name of the database to access
@@ -20,73 +15,14 @@ include "FINsert/opendb.php"; // ... which are used to open the db
 	}
 /*****************/
 
+// The file OpenMenu.xml contains an XML document with a root element
+// and at least an element /[root]/title.
 
-$cat; $rid; $bid;
-
-/**
- * $cat should be passed as a name-value pair using GET or POST. 
- * The value should be an array, condensed into the following form:
- * original array: [1,2,3,4,5]
- * $cat array    : "1 2 3 4 5"
- * Essentially, it is storing the array as a string
- */
-$cat = html_entity_decode($_REQUEST["cat"]);
-$cat_array = explode(" ", $cat);
-$rid = html_entity_decode($_REQUEST["rid"]);
-$bid = html_entity_decode($_REQUEST["bid"]);
-
-// Call and print the function if all parameters are passed
-if (isset($_REQUEST["cat"]) && isset($_REQUEST["rid"])) {
-    echo (get_object($rid, $bid, $cat_array));
+if (file_exists('OpenMenu.xml')) {
+    $xml = simplexml_load_file('OpenMenu.xml');
+ 
+    print_r($xml);
+} else {
+    exit('Failed to open OpenMenu.xml.');
 }
-
-/**
- * get_single_category function accepts a single category and outputs
- * in JSON the items associated with a given category and rid
- * All variables except $cat are used in the same fashion as get_object.
- * @returns: JSON string
- */
-function get_single_category($rid, $bid, $cat) {
-	
-	$singleBuilding = "";
-	
-	if ($bid > 0) {
-		$floor = ", f.name AS floor";
-		$building = "JOIN floors f ON i.fid = f.fid";
-		$singleBuilding = "AND f.bid = '$bid'";
-	}
-	 
-	$query = "SELECT *, i.item_id AS id $floor FROM items i "
-	       . "JOIN categories c ON i.cat_id = c.cat_id "
-	       . "$building "
-	       . "WHERE c.name = '$cat' AND i.rid = '$rid' "
-		   . "$singleBuilding LIMIT 0, 100";
-		   	       
-	$result = mysql_query($query);
-	if (!$result) {
-		echo mysql_error();
-		die ("MySQL encountered an error: " + mysql_error());
-	}
-	
-	$json_array = array();
-	while ($row = mysql_fetch_object($result)) {
-		$row_array = array( 
-			'id'   => (int)$row->id,
-			'lat'  => (int)$row->latitude,
-			'long' => (int)$row->longitude,
-			'info' => $row->special_info,
-			'fid' => (int)$row->fid,
-			'cat' => $cat,
-		);
-		// This is probably a good thing to have with an item.
-		if ($building_id) {
-	        	$row_array['bid'] = (int)$row->bid;
-		}
-		array_push($json_array, $row_array);    
-	}
-	
-	return json_encode($json_array); 
-	break; // probably unnecessary with return?
-}
-
-include "FINsert/closedb.php"; // close the db connection
+?>
