4b316e97919156b60775d2b9525c26422aab4bbf
Eric Hare <Raftysworld@gmail.com>
2012-01-20 10:16:11 -0800

    Convert the UI from a programmatic layout to an xml file

--- Android/OpenMenu/src/com/net/rmopenmenu/OpenMenuActivity.java
+++ Android/OpenMenu/src/com/net/rmopenmenu/OpenMenuActivity.java
@@ -2,15 +2,12 @@ package com.net.rmopenmenu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class OpenMenuActivity extends Activity {
    /** Called when the activity is first created. */
    @Override
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-       TextView tv = new TextView(this);
-       tv.setText("Eventually OpenMenu stuff will be here...");
-       setContentView(tv);
+       setContentView(R.layout.main);
    }
 }
