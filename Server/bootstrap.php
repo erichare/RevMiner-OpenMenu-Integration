cc83e85b22894869ac588dfdaa524a5300a35171
Eric Hare <Raftysworld@gmail.com>
2012-01-24 10:52:09 -0800

    New server code to load from the open menu database into our own automatically

---
+++ Server/bootstrap.php
@@ -0,0 +1,73 @@
+<?php
+
+/*******************/
+	$dbhost = 'localhost'; //The host server of the database
+	$dbname = 'dawgsfor_openmenu'; //The name of the database to access
+	$dbuser = 'dawgsfor_omuser'; //The user id for accessing the database
+	$dbpassword = '454ftw'; //the password for the user specified in dbuser
+	$db = mysql_connect($dbhost, $dbuser, $dbpassword);
+	if (!$db) {
+		die("Error, could not connect to server!" . mysql_error());
+	}
+		
+	if (!mysql_select_db($dbname)) {
+		die("Error, could not choose the db! " . mysql_error());
+	}
+/*****************/
+
+// The file OpenMenu.xml contains an XML document with a root element
+// and at least an element /[root]/title.
+
+if (file_exists('OpenMenu.xml')) {
+    $xml = simplexml_load_file('OpenMenu.xml');
+
+
+	foreach($xml->results->item as $item) {		 
+		$q = mysql_query("INSERT INTO restaurants (name, address, city, state, country) VALUES ('$item->restaurant_name', '$item->address_1', '$item->city_town', '$item->state_province', '$item->country')");
+			
+		if (!$q) {
+			print("Error, could not query the db! " . mysql_error());
+		} else {
+			print("You've successfully inserted a restaurant");
+		
+			$xml2 = simplexml_load_file($item->omf_file_url);
+			
+			foreach($xml2->menus->menu->menu_groups->menu_group as $menu_group) {
+				foreach($menu_group->menu_items->menu_item as $menu_item) {
+					$price = $menu_item->menu_item_price;
+					if ((string)$price == NULL)) {
+						$price = 0.00;
+					}
+
+					$q = mysql_query("INSERT INTO items (name, description, price) VALUES ('$menu_item->menu_item_name', '$menu_item->menu_item_description', $price)");
+				
+					if (!$q) {
+						print("Error, could not query the db! " . mysql_error());
+					} else {
+						print("You've successfully inserted an item");
+						
+						$q = mysql_query("SELECT rid FROM restaurants ORDER BY rid DESC LIMIT 1");			
+						$row = mysql_fetch_array($q);
+						$rid = $row["rid"];
+						
+						$q = mysql_query("SELECT iid FROM items ORDER BY iid DESC LIMIT 1");
+						$row = mysql_fetch_array($q);
+						$iid = $row["iid"];
+						
+						$q = mysql_query("INSERT INTO restaurants_items (rid, iid) VALUES ($rid, $iid)"); 
+						if (!$q) {
+							print("Error, could not query the db! " . mysql_error());
+						} else {
+							print("You've successfully inserted a menu item link");
+						}
+					}
+				}
+			}
+		}
+	}
+    
+    // SEND THIS INFORMATION TO THE DATABASE
+} else {
+    exit('Failed to open OpenMenu.xml.');
+}
+?>
---
+++ Server/sync.php
@@ -0,0 +1,103 @@
+<?php
+
+/*******************/
+	$dbhost = 'localhost'; //The host server of the database
+	$dbname = 'dawgsfor_openmenu'; //The name of the database to access
+	$dbuser = 'dawgsfor_omuser'; //The user id for accessing the database
+	$dbpassword = '454ftw'; //the password for the user specified in dbuser
+	$db = mysql_connect($dbhost, $dbuser, $dbpassword);
+	if (!$db) {
+		die("Error, could not connect to server!" . mysql_error());
+	}
+		
+	if (!mysql_select_db($dbname)) {
+		die("Error, could not choose the db! " . mysql_error());
+	}
+/*****************/
+
+$modified = "";
+if (isset($_REQUEST["timestamp"])) {
+	$modified = "WHERE modified > FROM_UNIXTIME($timestamp)";
+}
+
+// Call and print the function if all parameters are passed
+echo (get_restaurants($modified));
+echo (get_items($modified));
+echo (get_restaurants_items($modified));
+
+mysql_close($db);
+
+function get_restaurants($modified) {
+	 
+	$query = "SELECT * FROM restaurants $modified";
+		   	       
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error();
+		die ("MySQL encountered an error: " + mysql_error());
+	}
+	
+	$json_array = array();
+	while ($row = mysql_fetch_object($result)) {
+		$row_array = array( 
+			'rid'   => (int)$row->rid,
+			'name'  => (string)$row->name,
+			'address' => (string)$row->address,
+			'city' => (string)$row->city,
+			'state' => (string)$row->state,
+			'country' => (string)$row->country,
+		);
+
+		array_push($json_array, $row_array);    
+	}
+	
+	return json_encode($json_array); 
+}
+
+function get_items($modified) {
+	 
+	$query = "SELECT * FROM items $modified";
+		   	       
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error();
+		die ("MySQL encountered an error: " + mysql_error());
+	}
+	
+	$json_array = array();
+	while ($row = mysql_fetch_object($result)) {
+		$row_array = array( 
+			'iid'   => (int)$row->iid,
+			'name'  => (string)$row->name,
+			'description' => (string)$row->description,
+			'price' => (string)$row->price,
+		);
+
+		array_push($json_array, $row_array);    
+	}
+	
+	return json_encode($json_array); 
+}
+
+function get_restaurants_items($modified) {
+	 
+	$query = "SELECT * FROM restaurants_items $modified";
+		   	       
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error();
+		die ("MySQL encountered an error: " + mysql_error());
+	}
+	
+	$json_array = array();
+	while ($row = mysql_fetch_object($result)) {
+		$row_array = array( 
+			'rid'   => (int)$row->rid,
+			'iid'  => (int)$row->iid,
+		);
+
+		array_push($json_array, $row_array);    
+	}
+	
+	return json_encode($json_array); 
+}
