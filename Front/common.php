30a6151ec4826337d597b46a2e4677e02ffa4467
Donghyo Min <gajok@cs.washington.edu>
2012-01-25 16:13:53 -0800

    Front Page HTML5/php/img Files

---
+++ Front/common.php
@@ -0,0 +1,53 @@
+<?php
+# author: Donghyo Min, Eric Hare
+# filename: common.php
+function top(){
+    ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<!-- Donghyo Min, Eric Hare
+			 Very First Page for Restaurant Searcher -->
+	<head>
+		<title>Menu by Menu</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Web page that displays a playable JavaScript fifteen puzzle." />
+		<meta name="keywords" content="puzzle, fifteen" />
+
+		<!-- stop the web browser from ever caching this page or its images -->
+		<meta http-equiv="Cache-Control" content="no-cache" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />
+
+		<link href="index.css" type="text/css" rel="stylesheet" />
+		<link href="http://students.washington.edu/dongm/454/img/fifteen.gif" type="image/gif" rel="shortcut icon" />
+
+		<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.6.1.0/prototype.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+		<div id="bannerarea">
+			<br>Sick Foods For Sick people
+		</div>
+<?php
+}
+
+# This function is for checking validation and copyright.
+function bottom(){
+    ?>      
+    <div id="copyright">
+			<p>
+				Results and page (C) Copyright 2012 SFSP.
+			</p>
+		</div>
+
+		<div id="validator">
+			<a href="http://validator.w3.org/check/referer">
+				<img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid html" />
+			</a>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">
+				<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS" />
+			</a>
+		</div>
+	</body>
+</html>
+<?php } ?>
---
+++ Front/index.php
@@ -0,0 +1,41 @@
+<?php
+# Donghyo Min, Eric Hare
+# Front Page
+
+include("common.php");
+top();
+?>
+		<div>
+			<ul>				
+				<li>
+					<a href="match.php">
+						<img src="http://students.washington.edu/dongm/454/img/menu.gif" alt="icon" />
+						Find Restaurants By Menues !!!
+					</a>
+				</li>
+				
+				<li>
+					<a href="signup.php">
+						<img src="http://students.washington.edu/dongm/454/img/signup.gif" alt="icon" />
+						Add Good Menues You Know !!!
+					</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div id="copyright">
+			<p>
+				Results and page (C) Copyright 2012 SFSP.
+			</p>
+		</div>
+
+		<div id="validator">
+			<a href="http://validator.w3.org/check/referer">
+				<img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid html" />
+			</a>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">
+				<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS" />
+			</a>
+		</div>
+	</body>
+</html>
