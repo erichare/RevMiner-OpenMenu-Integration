e5bf63b58271a443bf01d23c28f111c0d385cc66
Donghyo Min <gajok@cs.washington.edu>
2012-01-27 02:32:32 -0800

    First version of match.php, which will use database

---
+++ Front/match.php
@@ -0,0 +1,10 @@
+<?php
+# Donghyo Min
+
+# This allows matches.php to use functions in common.php.
+include("common.php");
+# call function top() for the header logo.
+top();
+# bottom is for copyright sentence, and validation links to check.
+bottom();
+?>
