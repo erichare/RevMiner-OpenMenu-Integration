547df36f180daec5dba93c56a908a0f8d05185c0
Donghyo Min <gajok@cs.washington.edu>
2012-01-28 16:15:25 -0800

    trying to make a map better

--- Front/map.php
+++ Front/map.php
@@ -1,6 +1,6 @@
 <?php
-# author: Donghyo Min
-# filename: search.php
+# contact: gajok@cs.washington.edu
+# filename: map.php
 
 #include common.php for using top() and bottom() function.
 include("common.php");
@@ -24,37 +24,59 @@ if (isset($_REQUEST["long"])) {
 
 
 ?>
-<script type="text/javascript" src="http://www.google.com/jsapi?key=ABQIAAAAha3tZBPtoRIZLy8L__KptBQnG3kJyZKQJZVr4eR4kYFSiXUT9RRFxcXjJhP2jKzqq_rH2oTI3uqs_w&autoload=%7Bmodules%3A%5B%7Bname%3A%22maps%22%2Cversion%3A3%2Cother_params%3A%22sensor%3Dtrue%22%7D%5D%7D"></script>
-<script type="text/javascript" src="gears_init.js"></script>
-<div id="map" style="width: 800px; height: 300px"></div>
 
+<div id="map" style="width: 800px; height: 300px"></div>
 <script type="text/javascript">
-google.setOnLoadCallback(function() {
+
+	//google.setOnLoadCallback(function() {
+		// initialize the mobile map    
 	var map = new GMap2(document.getElementById("map"));
 	map.addControl(new GSmallMapControl());
 	map.addControl(new GMapTypeControl());
-	// initialize the mobile map    
-//	if (typeof(google.gears) != 'undefined') {
-	var geo = google.gears.factory.create('beta.geolocation');
-	geo.getCurrentPosition(function(position) {
+	
+	map.setCenter(new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>), 15);
+	var point = new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>);
+	map.addOverlay(new GMarker(point));
+/*
+	if (google.gears) {
+	alert("haha");
+		var geo = google.gears.factory.create('beta.geolocation');
+		
+		geo.getCurrentPosition(function(position) {
 			var lat = position.latitude;
 			var lng = position.longitude;
-			var position = new google.maps.LatLng(lat, lng);
-			map.addOverlay(new GMarker(position));
-        map.setOptions({
-					center: position,
-					zoom: 15
-					});
+			var point = new GLatLng(lat, lng);
+			
+		//	var position = new google.maps.LatLng(lat, lng);
+			map.setOptions({
+			//	center: position,
+				center: point,	
+				zoom: 15
 			});
-//	}
+		});
+		map.addOverlay(new GMarker(point));
+	}
+		/*
 
-	
-	
-//	map.setCenter(new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>), 15);
-//	var point = new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>);
-//	map.addOverlay(new GMarker(point));
+		if (typeof(google.gears) != 'undefined') {
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) {
+        var lat = position.latitude;
+        var lng = position.longitude;
+        var position = new google.maps.LatLng(lat, lng);
+        map.setOptions({
+            center: position,
+            zoom: 15
+        });
+    });
+}
+		*/
+		
+	//	map.setCenter(new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>), 15);
+	//	var point = new GLatLng(<?= $lat + 0 ?>, <?= $long + 0 ?>);
+	//	map.addOverlay(new GMarker(point));
 
-});
+	//});
 
 </script>
 
