02dde508570dbbbe87cd565bd9abbe79afc36098
Eric Hare <Raftysworld@gmail.com>
2012-02-13 18:44:45 -0800

    New server file to bootstrap database

---
+++ Server/run.php
@@ -0,0 +1,21 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>OpenMenu Bootstrap</title>
+		<link href="design.css" type="text/css" rel="stylesheet" />
+	</head>
+	
+	<body>
+		<div class = "content">
+		<h1>OpenMenu Extension Bootstrapper</h1>
+			<h2>Load Data From OpenMenu</h2>
+			
+			<p>
+				<a href ="bootstrap.php">Begin Bootstrap</a>
+			</p>
+		</div>
+
+	</body>
+
+
+</html>
