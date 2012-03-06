2322edfe255a66d68f1f65d6fe9e43ce7c0ae6c1
Donghyo Min <gajok@cs.washington.edu>
2012-03-06 05:01:47 -0800

    distance for each restaurant

--- Front/match.php
+++ Front/match.php
@@ -4,7 +4,7 @@
 
 #include common.php for using top() and bottom() function.
 include("common.php");
-
+session_start();
 # This function is for <head> part.
 top();
 
@@ -26,6 +26,12 @@ function do_query($query) {
    return $results;
 }
 
+function get_distance($lat, $lon){
+	
+	
+}
+
+
 # It gets Latitude and Longitude according to the address
 function get_lat_lon($row_sub) {
 	$q1 = str_replace(" ", "+", $row_sub[1]);
@@ -43,7 +49,6 @@ function get_lat_lon($row_sub) {
 
 function table_head_for_food($caption){
 ?>
-
 	<div id="table_start">
 		<div id="capture">
 			<?= $caption ?>
@@ -53,10 +58,9 @@ function table_head_for_food($caption){
 						<th>No.</th>
 						<th>Restaurant Name</th>
 						<th id="menu_name">menu name</th>
-						<!-- will add distance feature later 
-						<th>Distance</th> -->
-						<th>Map</th>
 						<th>Add to My Menu</th>
+						<th>Map</th>
+						<th>Distance(mi)</th>
 				</tr>
 <?php 
 }
@@ -84,7 +88,6 @@ function table_head_for_restaurant($caption){
 function menu_table($query_data, $food_name, $caption, $want){
     $result = do_query($query_data);
     $row = mysql_fetch_array($result);
-    #only debugging purpose. below line.
     $location = get_lat_lon($row);
     if(!$row){
         ?>
@@ -98,13 +101,8 @@ function menu_table($query_data, $food_name, $caption, $want){
 				?>
 					<div id="rest_info">
 						<p> address: <?= $row[2] ?> <?= $row[3] ?> <?= $row[4] ?> <?= $row[5] ?></p>
-						<p><!--
-							<button id="<?=$i?>" value="<?=($row[0])?>">See On A Map</button>
-							<script type="text/javascript">
-								var adding_map = document.getElementById("<?=$i?>");
-								adding_map.observe("click", get_lat_lon(<?= $row ?>));
-							</script> -->
-							<a href="map.php?lat=<?= $location["lat"] ?>&long=<?= $location["lon"] ?>" >
+						<p>
+							<a href="map.php?lat=<?= $row[8] ?>&long=<?= $row[9] ?>" >
 								<img src="http://www.project-fin.org/openmenu/Front/img/map_icon.jpg" alt="map_icon" />
 								See On a Map
 							</a>					
@@ -122,11 +120,10 @@ function menu_table($query_data, $food_name, $caption, $want){
 			
 					$i = 1;
 					while ($row) {
-					#	$location = get_lat_lon($row);
 						if($i % 2 == 0){
-								$zebra = "even";
-						} else {
 								$zebra = "odd";
+						} else {
+								$zebra = "even";
 						}
 						?>
 						
@@ -153,7 +150,6 @@ function menu_table($query_data, $food_name, $caption, $want){
 					
 					$i = 1;
 					while ($row) {
-					#	$location = get_lat_lon($row);
 						if($i % 2 == 0){
 								$zebra = "even";
 						} else {
@@ -161,24 +157,46 @@ function menu_table($query_data, $food_name, $caption, $want){
 						}
 						?>
 						
-						<tr class = "<?= $zebra ?>" >
+						<tr id = <?= $i ?> class = "<?= $zebra ?>" >
 							<td> <?= $i ?> </td>
 							<td> <?= htmlentities($row[1]) ?> </td>
 							<td> <?= htmlentities($row[0]) ?> </td>
-							<!-- will add distance feature later 
-							<td>0.0 mi</td> -->
+							<script type="text/javascript">
+								var add_but_sec = $(document.createElement("td"));
+								var add_but = $(document.createElement("button"));
+								add_but.innerHTML = "Add";
+								add_but.value = "<?=($row[0])?>";
+								
+								add_but.observe("click", add_food);
+								add_but_sec.appendChild(add_but);
+								
+								$("<?= $i ?>").appendChild(add_but_sec);
+								
+								if (typeof(navigator.geolocation) != 'undefined') {
+									navigator.geolocation.getCurrentPosition(function(position) {	// get current position.
+										var wp = new Array(2);		
+										var lat = position.coords.latitude;													// for user's current location: latitude
+										var lng = position.coords.longitude;												// for user's current location: longitude	
+										
+										var location1 = new GLatLng(lat, lng);
+										var location2 = new GLatLng(<?= $row[6] ?>, <?= $row[7] ?>);
+										var dist = location1.distanceFrom(location2);
+										dist = dist * 0.000621371192 + 0.1;		// convert meters to miles 
+										dist = Math.round(dist*100)/100 
+										
+										var mile = $(document.createElement("td"));
+										mile.innerHTML = dist;
+									
+										$("<?= $i ?>").appendChild(mile);
+										$("<?= $i ?>").appendChild(mile);
+									});
+								}
+							</script>
 							<td id="see_map"> 
-								<a href="map.php?lat=<?= $location["lat"] ?>&long=<?= $location["lon"] ?>" >
+								<a href="map.php?lat=<?= $row[6] ?>&long=<?= $row[7] ?>" >
 									<img src="http://www.project-fin.org/openmenu/Front/img/map_icon.jpg" alt="map_icon" />
 									See On a Map
 								</a>
-							</td>
-							<td>
-								<button id="<?=$i?>" value="<?=($row[0])?>">Add</button>
-								<script type="text/javascript">
-									var adding = document.getElementById("<?=$i?>");
-									adding.observe("click", add_food);
-								</script>
 							</td>
 						</tr>
 							
@@ -213,26 +231,29 @@ type_menu();
 if($want == "Restaurant"){
 	$cap = $menu." has the following menues.";
 } else if ($want == "Food"){
-	$cap = "The restaurants that has ".$menu; 
+	$cap = "The restaurants that has ".$want; 
 }
+
 
 if($want == "Restaurant"){
 	menu_table(
-		"SELECT i.name, r.name, r.address, r.city, r.state, r.country, i.description, i.price " .
+#	sorting_by_dist(
+		"SELECT i.name, r.name, r.address, r.city, r.state, r.country, i.description, i.price, r.lat, r.lon " .
 		"From restaurants r " .
 		"JOIN restaurants_items ri on ri.rid = r.rid " .
 		"JOIN items i on i.iid = ri.iid " .
 		"WHERE r.name LIKE ". "'%" . $menu . "%'".
-		" ORDER BY r.rid DESC;", $menu, $cap, $want
+		" ORDER BY i.name ASC;", $menu, $cap, $want
 	);
 } else if ($want == "Food"){
 	menu_table(
-		"SELECT i.name, r.name, r.address, r.city, r.state, r.country " .
+#	sorting_by_dist(
+		"SELECT i.name, r.name, r.address, r.city, r.state, r.country, r.lat, r.lon " .
 		"From restaurants r " .
 		"JOIN restaurants_items ri on ri.rid = r.rid " .
 		"JOIN items i on i.iid = ri.iid " .
 		"WHERE i.name LIKE ". "'%" . $menu . "%'".
-		" ORDER BY r.rid DESC;", $menu, $cap, $want
+		" ORDER BY r.name ASC;", $menu, $cap, $want
 	);
 }
 
